40edd37f23590f6c181294868ff726fd81087f81
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2019-06-19 14:54:03 -0400

    test

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhos
 exports.PORT = process.env.PORT || 3000;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-exports.CLIENT_ORIGIN = 'https://mighty-fortress-82471.herokuapp.com'
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
