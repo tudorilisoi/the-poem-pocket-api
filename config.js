98800041500dec40a09c6151c5265147730843ed
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2019-06-20 12:30:26 -0400

    updated env variable outputs

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/the-poem-pocket-api-db';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/the-poem-pocket-api-db-test';
 exports.PORT = process.env.PORT || 3000;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'TELLE_TUBBIES_123';
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3001';
--- server.js
+++ server.js
@@ -7,13 +7,7 @@ const passport = require('passport');
 const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
 
-// Here we use destructuring assignment with renaming so the two variables
-// called router (from ./users and ./auth) have different names
-// For example:
-// const actorSurnames = { james: "Stewart", robert: "De Niro" };
-// const { james: jimmy, robert: bobby } = actorSurnames;
-// console.log(jimmy); // Stewart - the variable name is jimmy, not james
-// console.log(bobby); // De Niro - the variable name is bobby, not robert
+
 const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const { router: poemRouter } = require('./poem-routing');
@@ -35,8 +29,19 @@ app.use(
 	})
 );
 
+app.use(function (req, res, next) {
+ 	res.header('Access-Control-Allow-Origin', '*');
+ 	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+ 	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+ 	if (req.method === 'OPTIONS') {
+ 		return res.send(204);
+ 	}
+ 	next();
+});
+
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
+app.use(passport.initialize());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
