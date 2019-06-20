ef11f170478f12e1e815416fec5e50068767dfdd
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2019-06-20 13:20:03 -0400

    updated jwt secret

--- server.js
+++ server.js
@@ -29,15 +29,6 @@ app.use(
 	})
 );
 
-app.use(function (req, res, next) {
- 	res.header('Access-Control-Allow-Origin', '*');
- 	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
- 	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
- 	if (req.method === 'OPTIONS') {
- 		return res.send(204);
- 	}
- 	next();
-});
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
